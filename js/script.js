f6e3a1fbff3e0ffc5752f3f9da4844c2a3105421
Paulo Guilherme Sousa Silva <paulogui@PG.local>
2020-10-02 14:59:48 -0300

    Reset board

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let lockBoard = false
 
 function flipCard() {
     if(lockBoard) return
+    if(this === firstCard) return
 
     this.classList.add('flip')
 
@@ -31,6 +32,8 @@ function checkForMath() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard()
 }
 
 function unflipCards() {
@@ -39,8 +42,13 @@ function unflipCards() {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
 
-        lockBoard = false
+        resetBoard()
     }, 1500)
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
 }
 
 cards.forEach((card) => {
